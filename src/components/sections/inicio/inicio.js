a872ce0e97c620cff81860502ad984386796da89
facumidvetkin <facumidvetkin@gmail.com>
2020-12-08 02:00:10 -0300

    Geocoding en Inicio, falta implementarlo al mapa

--- src/components/sections/inicio/inicio.js
+++ src/components/sections/inicio/inicio.js
@@ -1,13 +1,50 @@
 import React from 'react'
+import axios from 'axios'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
-const Inicio = (props) => {
+
+const getLoc = async(direc, callback) => {
+    const response = await axios('http://dev.virtualearth.net/REST/v1/Locations?q=candido%20pujato%203360%20santa%20fe%203000%20argentina&maxResults=1&key=Arn6kit_Moqpx-2p7jWVKy1h-TlLyYESkqc1cHzP1JkEAm1A_86T8o3FtDcKqnVV')
+    // const coords = response.data.resourceSets[0].resources[0].geocodePoints[0].coordinates
+    // console.log(typeof(coords[1]))
+    console.log(response.data)
+
+    return response.data.resourceSets[0].resources[0].geocodePoints[0].coordinates
+}
+
+
+const Inicio = () => {
+
+    const promise1 = getLoc().then(coords => {
+        return coords
+    });
+    console.log(promise1)
+
     return(
-        <h1 style={{textAlign: "center"}}>
-            Si estás leyendo esto, es porque sabes leer
-            <br/>
-            <br/>
-            No sé leer no sé qué dice 😭😭
-        </h1>
+        <div style={{width: '100%', height: '80vh'}}>
+            <h1 style={{textAlign: "center"}}>
+                Si estás leyendo esto, es porque sabes leer
+                <br/>
+                <br/>
+                No sé leer no sé qué dice 😭😭
+            </h1>
+
+            <div id="mapid" style={{width: '100%', height: '100%'}}>
+                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} style={{height:'100%'}}>
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {/* <Marker position={[-31.63176, -60.71262]}> */}
+                    <Marker position={[-31.63176, -60.71262]}>
+                        <Popup>
+                        Mi casa <br /> jeje
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
+
+        </div>
     )
 }
 
--- src/components/sections/util/webMapView.js
+++ src/components/sections/util/webMapView.js
@@ -7,18 +7,22 @@ export const WebMapView = () => {
     useEffect(
       () => {
         // lazy load the required ArcGIS API for JavaScript modules and CSS
-        loadModules(['esri/Map', 'esri/views/MapView'], { css: true })
+        loadModules(['esri/Map',
+         'esri/views/MapView'
+        ], 
+        { css: true })
         .then(([ArcGISMap, MapView]) => {
           const map = new ArcGISMap({
-            basemap: 'topo-vector'
+            basemap: 'streets'
           });
 
           // load the map view at the ref's DOM node
           const view = new MapView({
             container: mapRef.current,
             map: map,
-            center: [-118, 34],
-            zoom: 8
+            // center: [-118, 34],
+            center: [-60.7, -31.633333333333],
+            zoom: 13
           });
 
           return () => {
